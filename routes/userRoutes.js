850d035cb93db3b2f7c03a15fd51d3f1ea5df36d
Hariskumar S <hariskumar0704@gmail.com>
2025-03-14 23:25:29 +0530

    changes after deployment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express= require("express");
-const UserModel= require("./userRoutes.js");
+const UserModel= require("../models/userModel.js");
 const UserRouter = express.Router();
 
 UserRouter.get("/home",(req,res)=>{
